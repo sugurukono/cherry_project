0f73b9b0c925fe89ae34d212a251ec8efdb44637
Katsue Kawae <katsue0911@gmail.com>
2018-11-13 10:01:56 +0800

    headerだけのファイルを送ります。

---
+++ header_only.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css"  href="header_only.css">
+</head>
+<body>
+<!-- ヘッダー 開始-->
+  <div class="row">
+    <div class="col-xs-12" style="background-color: #003366; height: 90px">
+      <h1 class="title" style="color:white;">🍒Cherry</h1>
+
+    <li class="words">
+        <a href="#">My Page</a>
+    </li>
+     
+    <li class="words"><a href="#">Talk</a>
+    </li>
+
+
+    <li class="words">
+        <a href="#">Add Friends</a>
+    </li>
+     
+
+    <li class="words">
+        <a href="#">Setting</a>
+    </li>
+     
+     <li class="words">
+        <a href="#">Fake Page</a>
+    </li>
+
+    <li class="words">
+        <a href="#">Log Out</a>
+    </li>
+
+         </ul>
+    </div>
+  </div>
+
+
+
+
+</body>
+</html>
