8065ae90f16c7af4e9ccdf7b397ff4de0fca373d
Katsue Kawae <katsue0911@gmail.com>
2018-11-20 10:08:29 +0800

    編集

--- dbconnect.php
+++ dbconnect.php
@@ -1,11 +1,8 @@
 <?php
-
     $dsn = 'mysql:dbname=Cherry;host=localhost';
     $user = 'root';
     $password='';
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $dbh->query('SET NAMES utf8');
-
-
 ?>
