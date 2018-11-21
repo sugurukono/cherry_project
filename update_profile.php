69581305bcf40d8f7c1dc64badce15e9b6b168c7
西村結有 <bjarna.17@gmail.com>
2018-11-21 10:41:44 +0800

    update_profile.phpを作りました

---
+++ update_profile.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+
+    $sql = 'SELECT * FROM `users` WHERE `id`=?';
+    $data = array($_SESSION["id"]);
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+    $signin_user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (!empty($_POST)) {
+        $update_sql = "UPDATE `users` SET `user_img`=?,`user_name`=?,`search_id`=?,`email`=?,`comments`=? WHERE id=?";
+        $data = array($_POST["user_img"],$_POST["name"],$_POST["id"],$_POST["email"],$_POST["comments"]);
+        $stmt = $dbh->prepare($update_sql);
+        $stmt->execute($data);
+        header("Location: setting.php");
+        exit();
+
+    }
+
+?>
