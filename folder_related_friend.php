cd666d6d36a89646639c263d9fb1ff4eff18a97d
西村結有 <bjarna.17@gmail.com>
2018-12-02 09:32:49 +0800

    友達を二回同じファイルに登録すると二回表示されてしまうというバグを修正しました

--- folder_related_friend.php
+++ folder_related_friend.php
@@ -2,8 +2,8 @@
     session_start();
     require('dbconnect.php');
     require('functions.php');
-    v($_GET['check_folder'],'check_folder');
-    v($_SESSION['cherry']['related_friend'],"$_SESSION[cherry][related_friend]");
+    // v($_GET['check_folder'],'check_folder');
+    // v($_SESSION['cherry']['related_friend'],"$_SESSION[cherry][related_friend]");
 
 
     $friend_id=$_SESSION["cherry"]["related_friend"]["id"];
@@ -20,14 +20,25 @@
     $stmt->execute($data);
     $folder_name=$stmt->fetch(PDO::FETCH_ASSOC);
 
-    // v($folder_name,'$folder_name');
-
-
-    $sql='INSERT INTO `friends_folders` SET `folder_id`=?, `friend_id`=?, `folder_owner_id`=?, `created`=NOW();';
+    $sql = 'SELECT * FROM `friends_folders` WHERE `folder_id`=? AND `friend_id`=? AND `folder_owner_id`=?';
     $data= array($folder_name['id'],$friend_id,$folder_name['user_id']);
     $stmt = $dbh->prepare($sql);
     $stmt->execute($data);
-
+    $record=$stmt->fetch(PDO::FETCH_ASSOC);
+    // v($folder_name,'$folder_name');
+        if ($record == false) {
+            //初めてのフォルダ追加だったら
+            $sql='INSERT INTO `friends_folders` SET `folder_id`=?, `friend_id`=?, `folder_owner_id`=?, `created`=NOW();';
+            $data= array($folder_name['id'],$friend_id,$folder_name['user_id']);
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute($data);
+            //重複した友達追加だったら
+        }else{
+            $update_sql = 'UPDATE `friends_folders` SET `updated`= NOW() WHERE `folder_id`=? AND `friend_id`=? AND `folder_owner_id`=?';
+            $data = array($folder_name['id'],$friend_id,$folder_name['user_id']);
+            $stmt = $dbh->prepare($update_sql);
+            $stmt->execute($data);
+        }
 
     header('Location: setting.php');
     exit();
